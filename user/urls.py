8d8dec4cc3df4f453ea7f15d167a702734d2f9d3
pabaep <sewon225@gmail.com>
2022-02-17 21:09:37 +0900

    [update] 비밀번호 변경 코드

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import CreateView, LoginView
+from .views import CreateView, LoginView, ChangePw
 
 urlpatterns = [
     path('', views.index),
     path('create', CreateView.as_view()),
     path('login', LoginView.as_view()),
+    path('changepw', ChangePw.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -13,18 +13,21 @@ def index(request):
 class CreateView(View):
     def post(self, request):
         data = json.loads(request.body)
+        userName = data['userName']
+        userEmail = data['userEmail']
+        userPw = data['userPw']
         User(
-            userName = data['userName'],
-            userEmail = data['userEmail'],
-            userPw = data['userPw'],
+            userName = userName,
+            userEmail = userEmail,
+            userPw = userPw,
         )
 
-        if User.objects.filter(userEmail = data['userEmail']).exists() == True:
-            return JsonResponse({"message" : "이미 존재하는 이메일입니다."}, status = 401)
+        if User.objects.filter(userEmail=userEmail).exists():
+            return JsonResponse({"message" : "이미 존재하는 이메일입니다."}, json_dumps_params={'ensure_ascii': False}, status = 401)
 
         else:
-            User.objects.create(userEmail = data['userEmail'], userName = data['userName'], userPw = data['userPw'])
-            return JsonResponse({"message" : "회원으로 가입되셨습니다."}, status = 200)
+            User.objects.create(userEmail = userEmail, userName = userName, userPw = userPw)
+            return JsonResponse({"message" : "회원으로 가입되셨습니다."}, json_dumps_params={'ensure_ascii': False}, status = 200)
 
     def get(self, request):
         users = User.objects.values()
@@ -33,33 +36,54 @@ class CreateView(View):
 class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
-        # userName = ""
-        # uid = ""
-        # TODO 쿼리셋으로 데이터 보내주기
+
+        userEmail = data['userEmail']
+        userPw = data['userPw']
         User(
-            userEmail = data['userEmail'],
-            userPw = data['userPw'],
+            userEmail = userEmail,
+            userPw = userPw,
         )
 
-        if User.objects.filter(userEmail = data['userEmail']).exists() == True:
-            userData = User.objects.filter(userEmail = data['userEmail']).values('id','userName')[0]
+        if User.objects.filter(userEmail=userEmail).exists():
+            userData = User.objects.filter(userEmail = userEmail).values('id','userName')[0]
             print(userData.get('userName'))
 
             userName = userData.get('userName')
             uid = userData.get('id')
 
-        if User.objects.filter(userEmail = data['userEmail'],  userPw = data['userPw']).exists() == True :
+        if User.objects.filter(userEmail=userEmail, userPw=userPw).exists():
             return JsonResponse({"message": "로그인에 성공하셨습니다.",
-                                 "userEmail": data['userEmail'], "uid": uid, "userName":userName}, status = 200)
+                                 "userEmail": userEmail, "uid": uid, "userName":userName}, status = 200)
         else:
-            return JsonResponse({"message" : "아이디나 비밀번호가 일치하지 않습니다."}, status = 401)
+            return JsonResponse({"message" : "아이디나 비밀번호가 일치하지 않습니다."}, json_dumps_params={'ensure_ascii': False}, status = 401)
 
     def get(self, request):
         user = User.objects.values()
         return JsonResponse({"list" : list(user)}, json_dumps_params={'ensure_ascii': False}, status = 200)
 
+class ChangePw(View):
+    def patch(self, request):
+        data = json.loads(request.body)
+        newPw = data['newPw']
+        userEmail = data['userEmail']
+        userPw = data['userPw']
+        User(
+            userEmail = userEmail,
+            userPw = userPw
+        )
+
+        if User.objects.filter(userEmail=userEmail, userPw=userPw).exists():
+            userData = User.objects.filter(userEmail = userEmail)
+            # userData.update(userPw=newPw)
+            userData.update(userPw=newPw)
+
+            return JsonResponse({"message": "비밀번호가 변경되었습니다."}, json_dumps_params={'ensure_ascii': False}, status=200)
+        else:
+            return JsonResponse({"message" : "기존 비밀번호가 올바르지 않습니다."}, json_dumps_params={'ensure_ascii': False}, status=401)
 
 '''
 출처
 [  https://velog.io/@trequartista/TIL14-Django-회원가입로그인-기능-구현  ]
+https://jinmay.github.io/2020/05/13/django/django-queryset-update/
+https://tutorial.djangogirls.org/ko/django_orm/
 '''
